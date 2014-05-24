62f87fa0d9b86a4d75b33cf1ff519f2b07ba186f
aleph <aleph@riseup.net>
2014-05-24 09:41:38 +0200

    Add roles and caps management.

--- shop_worker.php
+++ shop_worker.php
@@ -19,16 +19,85 @@
  */
 
 
+ /*
+ * Redirect users who shouldn't be here.
+ * Hook fundify: _prevent_admin_access()
+ */
+function coopfy_prevent_admin_access() {
+
+	remove_action( 'admin_init', 'atcf_prevent_admin_access', 1000 );
+        if (
+                // Look for the presence of /wp-admin/ in the url
+                stripos( $_SERVER['REQUEST_URI'], '/wp-admin/' ) !== false
+                &&
+                // Allow calls to async-upload.php
+                stripos( $_SERVER['REQUEST_URI'], 'async-upload.php' ) == false
+                &&
+                // Allow calls to admin-ajax.php
+                stripos( $_SERVER['REQUEST_URI'], 'admin-ajax.php' ) == false
+        ) {
+		global $current_user;
+
+                if ( in_array( "campaign_contributor" , $current_user->roles ) ) {
+                        wp_safe_redirect( home_url() );
+                        exit();
+                }
+        }
+}
+add_action( 'admin_init', 'coopfy_prevent_admin_access' );
+
+
+/**
+ *
+ * Add shop_worker role, campaign_contributor capabilities
+ */
+function coopfy_add_caps() {
+
+	global $wp_roles;
+
+	if ( class_exists('WP_Roles') ) {
+	        if ( ! isset( $wp_roles ) )
+	                $wp_roles = new WP_Roles();
+	}
+
+	if ( is_object( $wp_roles ) ) {
+
+		// Clone campaign_contributor caps
+	        $fundify_caps = $wp_roles->get_role('campaign_contributor');
+		foreach ( $fundify_caps->capabilities as $key=>$value )
+	        	$wp_roles->add_cap( 'shop_worker', $key );
+
+		// add specific payments caps
+        	$wp_roles->add_cap( 'shop_worker', 'edit_shop_payments' );
+                $wp_roles->add_cap( 'shop_worker', 'edit_others_shop_payments' );
+
+		// remove some  no allowed caps
+		$wp_roles->remove_cap( 'shop_worker', 'manage_options' );
+                $wp_roles->remove_cap( 'shop_worker', 'publish_posts' );
+                $wp_roles->remove_cap( 'shop_worker', 'delete_published_posts' );
+                $wp_roles->remove_cap( 'shop_worker', 'edit_posts' );
+                $wp_roles->remove_cap( 'shop_worker', 'publish_products' );
+                $wp_roles->remove_cap( 'shop_worker', 'upload_files' );
+
+	}
+}
+register_activation_hook( __FILE__, 'coopfy_add_caps' );
+
+
 /**
  *
  * Hook fundify wp_insert_user registration to change user role.
  */
 function coopfy_registration_save( $user_id ) {
 
-        $user = get_user_by( "ID", $user_id );
+        $user = get_user_by( "id", $user_id );
 
-        if ( in_array( "campaings_contributor", $user->roles ) )
-                 wp_update_user ( $user_id, array( 'roles' => array ("shop_worker") ) );
+        if ( in_array( "campaign_contributor", $user->roles ) ) {
+
+        	$user->remove_role( 'campaign_contributor' );
+        	$user->add_role( 'shop_worker' );
+
+	}
 
 }
 add_action( 'user_register', 'coopfy_registration_save', 10, 1 );
@@ -44,7 +113,7 @@ add_action( 'user_register', 'coopfy_registration_save', 10, 1 );
 function coopfy_set_all_author( $wp_query ) {
         global $current_user;
 
-         if ( in_array( "shop_worker", $current_user->roles ) )
+        if ( in_array( "shop_worker", $current_user->roles ) )
                 $wp_query->set( 'author', '' );
 }
 
